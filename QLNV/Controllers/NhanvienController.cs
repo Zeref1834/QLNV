84cf892d2bc085ebe604f0a83cb626e7adc27eef
Tú Đỗ <anhtu1834@gmail.com>
2024-02-28 01:40:01 +0700

    .

--- QLNV/Controllers/NhanvienController.cs
+++ QLNV/Controllers/NhanvienController.cs
@@ -41,7 +41,7 @@ namespace QLNV.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> ImportDataAsync()
+        public async Task<IActionResult> ImportData()
         {
             //Kiểm tra có file hay không
             if (Request.Form.Files.Count == 0)
